a507c02fd9cadccd485147ef145c9ab5db58bd0b
jam_al <86470413+jambiebs@users.noreply.github.com>
2024-04-20 16:42:22 +0530

    Create monetag.js
    monetag code 1

---
+++ monetag.js
@@ -0,0 +1,1 @@
+(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://zovidree.com/tag.min.js',7370771,document.body||document.documentElement)
